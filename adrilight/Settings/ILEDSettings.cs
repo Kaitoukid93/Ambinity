35a26625a98c7ffd467096431984ac12b4b7d00e
Kaitoukid93 <saorihara93@gmail.com>
2021-09-26 03:42:52 +0700

    successfully save spot config to json database

---
+++ adrilight/Settings/ILEDSettings.cs
@@ -0,0 +1,19 @@
+﻿using adrilight.Spots;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adrilight
+{
+    public interface ILEDSettings
+    {
+        int DeviceID { get; set; }
+        string DeviceSerial { get; set; }
+        IDeviceSpot Spot0 { get; set; }
+
+       
+
+    }
+}
