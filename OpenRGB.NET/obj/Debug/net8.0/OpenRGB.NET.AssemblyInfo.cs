ebbffcb75e39b2f3ab9af584e80cbf51e2cc1b44
Kaitoukid93 <saorihara93@gmai.com>
2025-03-01 23:37:16 +0700

    add gitignore

--- OpenRGB.NET/obj/Debug/net8.0/OpenRGB.NET.AssemblyInfo.cs
+++ OpenRGB.NET/obj/Debug/net8.0/OpenRGB.NET.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2020 Diogo Trindade")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Client for the OpenRGB SDK")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("3.1.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("3.1.1+0c29a7e3ccdf03cbebe8ab96db5b0e4173469cab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("3.1.1+f08ece9e298c757869451389a84777334e77293a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenRGB.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenRGB.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("3.1.1.0")]
