80e0e5b0b70439b38853433410c87788b0498a23
Kaitoukid93 <saorihara93@gmai.com>
2025-05-23 03:10:24 +0700

    Change launch json configuration

--- OpenRGB.NET/obj/Debug/net8.0/OpenRGB.NET.AssemblyInfo.cs
+++ OpenRGB.NET/obj/Debug/net8.0/OpenRGB.NET.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2020 Diogo Trindade")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Client for the OpenRGB SDK")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("3.1.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("3.1.1+b86e88bc700d72e400d31871359d898e517c1da4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("3.1.1+ffbf2a0ffc5bf5a796415af8b7af8cdc08956936")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenRGB.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenRGB.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("3.1.1.0")]
